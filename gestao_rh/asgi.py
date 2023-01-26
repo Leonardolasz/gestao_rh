e853afa361fa9d52b78b062f9ef4582cc05ad037
leonardo alves <109185275+Leonardolasz@users.noreply.github.com>
2023-01-26 13:43:31 -0300

    criando a app departamentos

--- gestao_rh/asgi.py
+++ gestao_rh/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for restao_rh project.
+ASGI config for gestao_rh project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restao_rh.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestao_rh.settings')
 
 application = get_asgi_application()
--- gestao_rh/wsgi.py
+++ gestao_rh/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for restao_rh project.
+WSGI config for gestao_rh project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restao_rh.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestao_rh.settings')
 
 application = get_wsgi_application()
