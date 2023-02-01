16b6aa3015a8e4fbb1e12107f0adcd61c88e054e
leonardo alves <109185275+Leonardolasz@users.noreply.github.com>
2023-02-01 13:44:56 -0300

    Crud de empresas e funcionários

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path, include
-from .views import home
+from .views import (
+    FuncionariosList,
+    FuncionarioEdit,
+    FuncionarioDelete,
+    FuncionarioCreate)
 
 urlpatterns = [
-    path('', home)
+    path('', FuncionariosList.as_view(), name='list_funcionarios'),
+    path('editar/<int:pk>', FuncionarioEdit.as_view(), name='update_funcionario'),
+    path('novo/', FuncionarioCreate.as_view(), name='create_funcionario'),
+    path('apagar/<int:pk>', FuncionarioDelete.as_view(), name='delete_funcionario'),
 ]
--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,5 +1,48 @@
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import (
+    ListView,
+    UpdateView,
+    DeleteView,
+    CreateView)
 from django.shortcuts import render
+from .models import Funcionario
 
-def home(request):
-    return HttpResponse('Olá')
+
+@method_decorator(login_required, name='dispatch')
+class FuncionariosList(ListView):
+    model = Funcionario
+
+    def get_queryset(self):
+        try:
+            self.funcionario = self.request.user.funcionario
+            empresa_logada = self.funcionario.empresa
+            funcionarios = Funcionario.objects.filter(empresa=empresa_logada)
+            return funcionarios
+        except Funcionario.DoesNotExist:
+            return render(self.request, 'funcionarios/funcionario_does_not_exist.html', status=404)
+
+
+class FuncionarioEdit(UpdateView):
+    model = Funcionario
+    fields = ['nome', 'departamentos']
+
+
+class FuncionarioDelete(DeleteView):
+    model = Funcionario
+    success_url = reverse_lazy('list_funcionarios')
+
+
+class FuncionarioCreate(CreateView):
+    model = Funcionario
+    fields = ['nome', 'departamentos']
+
+    def form_valid(self, form):
+        funcionario = form.save(commit=False)
+        username = funcionario.nome.split(' ')[0] + funcionario.nome.split(' ')[0]
+        funcionario.empresa = self.request.user.funcionario.empresa
+        funcionario.user = User.objects.create(username=username)
+        funcionario.save()
+        return super(FuncionarioCreate, self).form_valid(form)
