f1abe19acfc79491a544da2cb776be8f4e9f3582
leonardo alves <109185275+Leonardolasz@users.noreply.github.com>
2023-02-03 15:16:44 -0300

    Crud de documentos e registro de horas extras.

--- apps/documentos/views.py
+++ apps/documentos/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import Documentos
 
-# Create your views here.
+
+class DocumentoCreate(CreateView):
+    model = Documentos
+    fields = ['descricao', 'arquivo']
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        form.instance.pertence_id = self.kwargs['funcionario_id']
+
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        obj = form.save()
+        funcionario = self.request.user.funcionario
+        funcionario.documentos = obj
+        self.object = obj
+        funcionario.save()
+        return super(DocumentoCreate, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['object'] = self.object
+        return context
+
+
+
+
--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,6 +8,8 @@ urlpatterns = [
     path('funcionarios/', include('apps.funcionarios.urls')),
     path('departamentos/', include('apps.departamentos.urls')),
     path('empresa/', include('apps.empresas.urls')),
+    path('documentos/', include('apps.documentos.urls')),
+    path('hora-extra/', include('apps.registro_hora_extra.urls')),
     path('', include('apps.core.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
